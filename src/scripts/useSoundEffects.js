b8a38c1177288bfb45b82c9b3c4f18c7f93f6ec1
wAnnOstrowska <w.annostrowska@gmail.com>
2026-02-07 17:13:04 +0100

    audio fix

--- src/scripts/useSoundEffects.js
+++ src/scripts/useSoundEffects.js
@@ -3,12 +3,22 @@ export function useSoundEffects() {
   const carCrashPath = new URL('../assets/sounds/carcrash.wav', import.meta.url).href;
   const noSoundPath = new URL('../assets/sounds/sus.mp3', import.meta.url).href;
 
-  const yippie = new URL('../assets/sounds/yippie.mp3', import.meta.url).href
-  const kiss = new URL('../assets/sounds/kiss.mp3', import.meta.url).href
+  const yippiePath = new URL('../assets/sounds/yippie.mp3', import.meta.url).href
+  const kissPath = new URL('../assets/sounds/kiss.mp3', import.meta.url).href
+
+  const carCrashAudio = new Audio(carCrashPath);
+  carCrashAudio.preload = 'auto';
+  const noSoundAudio = new Audio(noSoundPath);
+  noSoundAudio.preload = 'auto';
+
+  const yippieAudio = new Audio(yippiePath);
+  yippieAudio.preload = 'auto';
+  const kissAudio = new Audio(kissPath);
+  kissAudio.preload = 'auto';
 
   const playNoSound = (count) => {
     // Wybieramy dźwięk na podstawie licznika
-    const source = count === 0 ? carCrashPath : noSoundPath;
+    const source = count === 0 ? carCrashAudio : noSoundAudio;
     const audio = new Audio(source);
     
     audio.volume = 0.3; // Ustaw głośność, żeby nie ogłuchnąć ;)
@@ -17,7 +27,7 @@ export function useSoundEffects() {
   };
 
   const playYesSound = (count) => {
-    const source = count === 3 ? kiss : yippie;
+    const source = count === 3 ? kissAudio : yippieAudio;
     const audio = new Audio(source);
 
     audio.volume = 0.3;
